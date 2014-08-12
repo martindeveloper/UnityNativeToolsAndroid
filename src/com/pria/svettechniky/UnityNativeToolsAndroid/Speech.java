76d6246cd41abe6b1be8fe55b942d69874dd5d1a
Martin Pernica <pernica@pria.cz>
2014-08-12 17:49:00 +0200

    First version

---
+++ src/com/pria/svettechniky/UnityNativeToolsAndroid/Speech.java
@@ -0,0 +1,33 @@
+package com.pria.svettechniky.UnityNativeToolsAndroid;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
+import java.util.Locale;
+
+
+public class Speech {
+	static TextToSpeech tts;
+	
+	public static void SayText(Context context, final String text)
+	{
+		Log.i("UnityNativeToolsAndroid", "Context is " + ((context == null) ? "null" : "not null"));
+		
+		tts = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+			   @Override
+			   public void onInit(int status) {
+					Log.i("UnityNativeToolsAndroid", "TTS init status " + status);
+					
+					if(status == TextToSpeech.ERROR)
+					{
+						Log.i("UnityNativeToolsAndroid", "TextToSpeech init failed with error");
+					}else{
+						tts.setLanguage(Locale.ENGLISH);
+						tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+					}
+			   }
+			}
+		);
+	}
+}
