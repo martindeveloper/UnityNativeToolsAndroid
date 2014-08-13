adeb85ed3aa75621b567f8516495643fe12776f6
Martin Pernica <pernica@pria.cz>
2014-08-13 11:45:25 +0200

    Refactored code from static class

--- src/com/UnityNativeToolsAndroid/Speech.java
+++ src/com/UnityNativeToolsAndroid/Speech.java
@@ -2,32 +2,62 @@ package com.UnityNativeToolsAndroid;
 
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
 
 import java.util.Locale;
 
 
-public class Speech {
-	static TextToSpeech tts;
+public class Speech implements OnInitListener {
+	public TextToSpeech tts;
+	public Context context;
+	public boolean isTtsReady; 
 	
-	public static void SayText(Context context, final String text)
+	private static final String TAG = "UnityNativeToolsAndroid";
+	
+	public Speech(Context context)
 	{
-		Log.i("UnityNativeToolsAndroid", "Context is " + ((context == null) ? "null" : "not null"));
+		this.setContext(context);
 		
-		tts = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
-			   @Override
-			   public void onInit(int status) {
-					Log.i("UnityNativeToolsAndroid", "TTS init status " + status);
-					
-					if(status == TextToSpeech.ERROR)
-					{
-						Log.i("UnityNativeToolsAndroid", "TextToSpeech init failed with error");
-					}else{
-						tts.setLanguage(Locale.ENGLISH);
-						tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-					}
-			   }
-			}
-		);
+		tts = new TextToSpeech(this.getContext(), this);
+	}
+	
+	public Context getContext() {
+		return context;
+	}
+
+	public void setContext(Context context) {
+		Log.i(TAG, "Context is " + ((context == null) ? "null" : "not null"));
+		
+		this.context = context;
+	}
+
+	public boolean speak(final String text)
+	{
+		if(isTtsReady){
+			tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+			
+			return true;
+		}
+		
+		Log.e(TAG, "Can not call speak method, because TTS is not ready.");
+		
+		return false;
+	}
+
+	@Override
+	public void onInit(int status) {
+		Log.i(TAG, "TTS init status " + status);
+		
+		if(status == TextToSpeech.ERROR)
+		{
+			Log.e(TAG, "TextToSpeech init failed with error");
+			
+			isTtsReady = false;
+		}else{
+			tts.setLanguage(Locale.ENGLISH);
+			
+			isTtsReady = true;
+		}
 	}
 }
